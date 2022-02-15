d0ee8d2b0b83d29ff8db65369ca9d281029a54d2
Gustavo Mauricio <gmauricio207@gmail.com>
2022-02-15 18:23:27 -0300

    Criado os testes da camada Controller para o create

--- src/controllers/taskControllers.js
+++ src/controllers/taskControllers.js
@@ -1,16 +1,19 @@
 const res = require('express/lib/response');
 const {
-	createTaskService, getAllService,
+	createTaskService,
+  //  getAllService,
 } = require('../services/taskService');
 
 const createTaskController = async (req, res) => {
   const response = await createTaskService(req.body);
-  const { message, status, result } = response;
+
+  const { message, status } = response;
 
   if (message) {
-    return res.status(status).json(message)
+    return res.status(status).json({ message });
   } 
-	 return res.status(201).json(result);
+  
+	 return res.status(201).json( response );
 };
 
 // const getAllTaskController = async (req, res) => {
--- src/tests/controllers/taskControllerCreate.test.js
+++ src/tests/controllers/taskControllerCreate.test.js
@@ -11,8 +11,8 @@ const taskService = require('../../services/taskService');
 // Retornar o código de status 201 - Created ;
 // Retornar a mensagem Filme criado com sucesso! .
 
-describe('Ao chamar o taskCreateController', () => {
-  describe('Quando o payload informado não é válido', () => {
+describe('When calling the taskCreateController', () => {
+  describe('When the payload report is not valid', () => {
 		const res = {};
 		const req = {};
 
@@ -21,52 +21,55 @@ describe('Ao chamar o taskCreateController', () => {
 
 			res.status = sinon.stub().returns(res);
 			res.json = sinon.stub().returns();
+
+			sinon.stub(taskService,'createTaskService').resolves({ status: 'status', message:'message' })
 		});
 
 		after(() => {
 			taskService.createTaskService.restore();
 		});
 
-		it('é chamado o status com o código 400', async() => {
+		it('when task is not sent return request with code "400"', async() => {
+
+			req.body = { task: '', status: 'pendente'};
 			await taskController.createTaskController(req, res);
 
 			expect(res.status.calledWith(400)).to.be.equal(true);
 		});
 
-		it('O json é chamado com a messagem "O campo não pode estar vazio"', async() => {
+		it('The json calling with message "O campo não pode estar vazio"', async() => {
 			await taskController.createTaskController(req, res);
 
 			expect(res.json.calledWith({ message: 'O campo não pode estar vazio'})).to.be.equal(true);
 		});
 	});
 
-	describe('Quando é inserido com sucesso', () => {
+	describe('When it is successfully entered', () => {
 		const res = {};
 		const req = {};
 
 		before(() => {
 			req.body = {
 				task: "Tarefa 01",
+				status: 'pendente',
 			};
 
 			res.status = sinon.stub().returns(res);
 			res.json = sinon.stub().returns();
+
+			sinon
+			.stub(taskService, 'createTaskService')
+			.resolves({id:'154545454', task: 'Tarefa 01', status: 'pendente'});
 		});
 
 		after(() => {
 			taskService.createTaskService.restore();
 		});
 
-		it('É chamado com o status 201', async() => {
+		it('is called with status "201"?', async () => {
 			await taskController.createTaskController(req,res);
-
+console.log(res.status);
 			expect(res.status.calledWith(201)).to.be.equal(true);
 		});
-
-	// 	it('O json é chamado com o ID criado', async () => {
-	// 		await taskController.createTaskController(req, res);
-
-	// 		expect(res.json.calledWith({ message: ID_RESULT })).to.be.equal(true);
-	// 	})
-	 })
-})
+	 });
+});
